7efc4bc3d04791db8133729b774a23f9a34aad91
TebyTeb <esteban.c.ojedahdez@gmail.com>
2022-11-22 18:28:38 +0000

    Styling

--- Hotelerio/src/views/PersonalView.vue
+++ Hotelerio/src/views/PersonalView.vue
@@ -4,21 +4,31 @@ import PersonalButton from '../components/PersonalButton.vue'
 
 <script>
 export default {
-    data() {
-        return {}
-    }
+  data() {
+    return {}
+  }
 }
 </script>
 
 <template>
+  <div class="container">
     <br>
     <br>
     <h1>Personal Page</h1>
     <div>
-        <PersonalButton text="Make a Reservation" link="checkAvailableRooms" />
+      <PersonalButton text="Make a Reservation" link="checkAvailableRooms" />
     </div>
     <br>
     <div>
-        <PersonalButton text="Type of Rooms" link="home" />
+      <PersonalButton text="Type of Rooms" link="home" />
     </div>
+  </div>
 </template>
+
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
