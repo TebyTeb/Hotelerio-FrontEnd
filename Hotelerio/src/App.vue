ceef22799e5f7fa8834eb568881e8d9521ab8340
TebyTeb <esteban.c.ojedahdez@gmail.com>
2022-11-23 18:28:42 +0000

    ADDED: Pinia implementation STYLING: Different bg depending on loggedIn state

--- Hotelerio/src/App.vue
+++ Hotelerio/src/App.vue
@@ -2,12 +2,25 @@
 import { RouterLink, RouterView } from 'vue-router'
 import NavBar from '../src/components/NavBar.vue'
 import Footer from '../src/components/Footer.vue'
+import { useAuthStore } from './stores/store'
+import { def } from '@vue/shared';
+</script>
+
+<script>
+export default {
+  data() {
+    return {
+      store: useAuthStore(),
+      loggedIn: true
+    }
+  },
+}
 </script>
 
 <template>
-  <main class="view bg-img">
-    <NavBar class="navbar"/>
-    <RouterView class="router-view"/>
+  <main class="view" :class="{ 'bg-img-blur': store.isLoggedIn === true, 'bg-img': store.isLoggedIn === false}">
+    <NavBar class="navbar" />
+    <RouterView class="router-view" />
     <Footer />
   </main>
 </template>
@@ -19,19 +32,28 @@ import Footer from '../src/components/Footer.vue'
   height: 100%;
   min-height: 100vh;
 }
+
 .router-view {
   flex: 1 0 auto;
   margin-top: 100px;
-margin-bottom: 50px;
+  margin-bottom: 50px;
 
 }
-.bg-img {
+
+.bg-img-blur {
   background-image: url(./assets/images/reception/recepcion-blur.png);
   background-size: cover;
   background-position: center;
 }
-.navbar{
-  position:fixed;
-  top:0;
+
+.bg-img {
+  background-image: url('./assets/images/reception/reecpcion.jpg');
+  background-size: cover;
+  background-position: center;
+}
+
+.navbar {
+  position: fixed;
+  top: 0;
 }
 </style>
